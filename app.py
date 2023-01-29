e5263357102ec4266bad2176f148912781b33ef6
LikithaGarla <60319213+LikithaGarla@users.noreply.github.com>
2023-01-29 23:39:08 +0530

    Update app.py
    Added few enhancements

--- app.py
+++ app.py
@@ -1,26 +1,29 @@
+%%writefile app.py
 import streamlit as st
-import snscrape.modules.twitter as sntwitter
 import pandas as pd
 import snscrape.modules.twitter as sntwitter
 import itertools
 import pymongo
 from pymongo import MongoClient
 import datetime
-import pyautogui
+from google.colab import files
+from time import sleep
 
-global collection
 data = pd.DataFrame()
 
 def InitializeConnection():
   global collection
-  uri = 'mongodb://MongoDb:<password>@ac-jdflryq-shard-00-00.ciatbst.mongodb.net:27017,ac-jdflryq-shard-00-01.ciatbst.mongodb.net:27017,ac-jdflryq-shard-00-02.ciatbst.mongodb.net:27017/?ssl=true&replicaSet=atlas-kn8q2t-shard-0&authSource=admin&retryWrites=true&w=majority'
+  uri = 'mongodb://MongoDb:MongoDbGUVI@ac-jdflryq-shard-00-00.ciatbst.mongodb.net:27017,ac-jdflryq-shard-00-01.ciatbst.mongodb.net:27017,ac-jdflryq-shard-00-02.ciatbst.mongodb.net:27017/?ssl=true&replicaSet=atlas-kn8q2t-shard-0&authSource=admin&retryWrites=true&w=majority'
   conn = MongoClient( uri )
   db = conn.likitha
   collection = db.ScrapeData
 
 def Upload_to_Database():
+  placeholder.empty()
   data_list = data.values.tolist()
+  recordKey = word+'_'+str(datetime.datetime.now().timestamp())
   collection.insert_one({word : data_list})
+  placeholder.write("Successfully Uploaded to Database")
 
 def Twitter_Scrap(search_text,from_date,to_date,count):
   global data
@@ -32,27 +35,38 @@ def Twitter_Scrap(search_text,from_date,to_date,count):
   
 def Search_Button_Clicked():
   data =  Twitter_Scrap(word,from_date,to_date,limit_number)
-  pyautogui.hotkey("ctrl","F5")
-
-def Click():
-  st.write('Button is Clicked')
-
-def Display_Data():
-  st.write(data.values)
-  if st.button("Upload to Database"):
-    Upload_to_Database()
-  if st.button("Download as CSV"):
-    data.to_csv('SampleFile.csv')
-  if st.button("Download as Json"):
-    data.to_json('SampleFile1.json')
-    
 
 InitializeConnection()
 st.title('Twitter Data Scrapping')
-word = st.text_input('Enter Keyword or Hashtag to search')
-from_date = st.date_input('Enter the starting date range')
-to_date = st.date_input('Enter the ending date')
-limit_number = st.number_input('Enter no. of records to search', 0, 5000)
-if st.button("Search Data"):
-  Search_Button_Clicked()
-  Display_Data()
+placeholder = st.empty()
+flag=0
+with placeholder.container():
+  word = st.text_input('Enter Keyword or Hashtag to search')
+  from_date = st.date_input('Enter the starting date range')
+  to_date = st.date_input('Enter the ending date')
+  limit_number = st.number_input('Enter no. of records to search', 0, 5000)
+  if st.button("Search Data"):
+    placeholder.empty()
+    Search_Button_Clicked()
+    flag=1
+if flag==1:
+  placeholder.empty()
+  sleep(0.01)
+  with placeholder.container():
+    col1, col2, col3 = st.columns([1,1,1])
+    with col1:
+      st.button("Upload to Database",key=None,on_click=Upload_to_Database)
+    with col2:
+      st.download_button(
+        label="Download as CSV",
+        data=data.to_csv().encode('utf-8'),
+        file_name=word+'.csv',
+        mime='text/csv')
+    with col3:
+      st.download_button(
+        label="Download as Json",
+        data=data.to_json().encode('utf-8'),
+        file_name=word+'.json',
+        mime='application/json')
+    st.table(data)
+
